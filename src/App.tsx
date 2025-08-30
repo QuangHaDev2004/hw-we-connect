77d15171952c65abdfdb5ab8a689eb7fc9e688d4
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-08-30 09:29:09 +0700

    Init Project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <h1 className='bg-amber-500 font-bold'>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
