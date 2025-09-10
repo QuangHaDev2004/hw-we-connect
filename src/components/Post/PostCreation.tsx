6ae66bc7cd335b8dda22f7c42fcfedf39fabd9fa
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-09-10 15:10:59 +0700

    improve UI creation post

--- src/components/Post/PostCreation.tsx
+++ src/components/Post/PostCreation.tsx
@@ -3,6 +3,7 @@ import { useUserInfo } from "../../hooks/useUserInfo";
 import { Dialog } from "../Dialog/Dialog";
 import { Avatar } from "../Avatar/Avatar";
 import { useDropzone } from "react-dropzone";
+import { FaXmark } from "react-icons/fa6";
 
 export const PostCreation = () => {
   const userInfo = useUserInfo();
@@ -10,11 +11,9 @@ export const PostCreation = () => {
   const [image, setImage] = useState<File | null>(null);
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    // Do something with the files
-
-    console.log(acceptedFiles);
     setImage(acceptedFiles[0]);
   }, []);
+  
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     multiple: false,
@@ -41,8 +40,14 @@ export const PostCreation = () => {
           onFocus={() => setIsOpen(true)}
         />
         <Dialog isOpen={isOpen} onClose={() => setIsOpen(false)}>
-          <h3 className="border-three border-b pb-4 text-[16px] font-bold">
-            Create Post
+          <h3 className="border-three flex items-center justify-between border-b pb-4 text-[16px] font-bold">
+            <span>Create Post</span>
+            <button
+              onClick={() => setIsOpen(false)}
+              className="bg-three flex h-8 w-8 cursor-pointer items-center justify-center rounded-full"
+            >
+              <FaXmark className="text-secondary text-[14px]" />
+            </button>
           </h3>
           <div className="flex items-center gap-3 py-3">
             <Avatar />
@@ -50,7 +55,10 @@ export const PostCreation = () => {
           </div>
           <textarea className="border-three h-[150px] w-full resize-none rounded-md border p-4"></textarea>
 
-          <div {...getRootProps()}>
+          <div
+            {...getRootProps()}
+            className="bg-three mt-2 cursor-pointer rounded-md p-8 text-center"
+          >
             <input {...getInputProps()} />
             {isDragActive ? (
               <p>Drop the files here ...</p>
@@ -58,13 +66,21 @@ export const PostCreation = () => {
               <p>Drag 'n' drop some files here, or click to select files</p>
             )}
           </div>
-          <p>{image?.name}</p>
+          {image?.name && (
+            <div className="border-three mt-2 flex items-center justify-center gap-4 rounded-md border bg-slate-100 p-2">
+              <div className="">{image.name}</div>
+              <button
+                onClick={() => setImage(null)}
+                className="flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-red-500 text-white"
+              >
+                <FaXmark className="text-[14px]" />
+              </button>
+            </div>
+          )}
 
-          <div className="mt-4 w-full text-right">
-            <button className="bg-primary h-10 w-20 cursor-pointer rounded-md text-center text-[14px] font-bold text-white uppercase">
-              POST
-            </button>
-          </div>
+          <button className="bg-primary mt-4 h-10 w-full cursor-pointer rounded-md text-center text-[14px] font-bold text-white uppercase">
+            POST
+          </button>
         </Dialog>
       </div>
     </>
